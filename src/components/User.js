a826982cdd8086852b2ff66948ddf6f729653b9f
as87053 <as87053@lamatechnology.com>
2021-11-22 23:01:28 -0300

    Fixed user component

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,10 @@
+import axios from "axios";
 import React from "react";
 import { Button, Card } from "react-bootstrap";
 import '../App.css';
+import { useHistory } from 'react-router-dom';
+
+const BASE_URL = "http://localhost:3030/users";
 
 const User = ({
     uuid,
@@ -9,17 +13,34 @@ const User = ({
     createdAt,
     updatedAt
 }) => {
+
+    const history = useHistory();
+
+    const handleRemoveUser = async (userUuid) =>{
+        console.log(userUuid)
+        await axios.delete(BASE_URL + "/" + userUuid).then((response) => {
+            window.location.reload();
+        }).catch((error) => {
+            console.log(error)
+        });
+    }
+
+    const formatDate = (date) =>{
+        const dateObject = new Date(date);
+        return `${dateObject.getDate()}/${dateObject.getMonth() + 1}/${dateObject.getFullYear()} - ${dateObject.getHours()}:${dateObject.getMinutes()}:${dateObject.getSeconds()}`
+    }
+
     return (
-        <Card style={{width: '18rem'}} className="user">
+        <Card style={{width: '18rem', marginTop: '20px'}} className="user">
             <Card.Body>
                 <Card.Title className="user-name">{username}</Card.Title>
                 <div className="user-details">
                     <div>Age: {age}</div>
-                    <div>Created at: {new Date(createdAt).toLocaleDateString()}</div>
-                    <div>Updated at: {new Date(updatedAt).toLocaleDateString()}</div>
+                    <div>Created at: {formatDate(createdAt)}</div>
+                    <div>Updated at: {formatDate(updatedAt)}</div>
                 </div>
-                <Button variant="primary">Edit</Button>{' '}
-                <Button variant="danger">Delete</Button>
+                <Button variant="primary" onClick={() => history.push(`/edit/${uuid}`)}>Edit</Button>{' '}
+                <Button variant="danger" onClick={() => handleRemoveUser(uuid)}>Delete</Button>
             </Card.Body>
         </Card>    
     )
